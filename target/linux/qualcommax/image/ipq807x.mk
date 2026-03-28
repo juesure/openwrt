41d1609581b52ef3eb5d390ce0cc453f45e17a26
juesure <249899074@qq.com>
2026-03-28 22:37:59 +0800

    Update ipq807x.mk
    Signed-off-by: juesure <249899074@qq.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -1,13 +1,8 @@
 include $(TOPDIR)/rules.mk
 include $(INCLUDE_DIR)/image.mk
 
-# 删掉了不存在的 common-ipq807x.mk
-# 删掉了错误的 common.mk
-# 完全使用原生结构，无任何警告
-
 DEVICE_VARS += QCOM_SOC_ROM QCOM_DTS_ROM QCOM_ART_ROM
 
-# 保留你原版 Redmi AX6 256M 配置
 define Device/redmi_ax6
   $(call Device/xiaomi_ax3600)
   DEVICE_VENDOR := Redmi
