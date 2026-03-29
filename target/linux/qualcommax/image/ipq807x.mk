9d584572355dd40a811776a246670bc0832afc8d
juesure <249899074@qq.com>
2026-03-29 11:03:35 +0800

    Update ipq807x.mk
    Signed-off-by: juesure <249899074@qq.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -3,13 +3,45 @@ include $(INCLUDE_DIR)/image.mk
 
 DEVICE_VARS += QCOM_SOC_ROM QCOM_DTS_ROM QCOM_ART_ROM
 
+define Device/FitImage
+endef
+
+define Device/UbiFit
+endef
+
+define Device/xiaomi_ax3600
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX3600
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac04
+	SOC := ipq8071
+	KERNEL_SIZE := 36608k
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax3600 kmod-ath10k-ct-smallbuffers ath10k-firmware-qca9887-ct
+ifeq ($(IB),)
+ifneq ($(CONFIG_TARGET_ROOTFS_INITRAMFS),)
+	ARTIFACTS := initramfs-factory.ubi
+	ARTIFACT/initramfs-factory.ubi := append-image-stage initramfs-uImage.itb | ubinize-kernel
+endif
+endif
+endef
+TARGET_DEVICES += xiaomi_ax3600
+
+# ===================== Redmi AX6 配置（IPQ8071A） =====================
 define Device/redmi_ax6
-  SOC := ipq8071
-  DEVICE_DTS := ipq8071-ax6
-  DEVICE_VENDOR := Redmi
-  DEVICE_MODEL := AX6
-  IMAGE_SIZE := 245760k
-  DEVICE_PACKAGES := ipq-wifi-redmi_ax6
+	$(call Device/xiaomi_ax3600)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX6
+	IMAGE_SIZE := 245760k
+	UBINIZE_OPTS := -E 5 -m 2048 -p 128KiB -s 2048 -O 2048
+	KERNEL_IN_UBI := 1
+	IMAGES += factory.ubi
+	IMAGE/factory.ubi := append-ubi | check-size $$$$(IMAGE_SIZE)
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 \
+		kmod-ath11k-ahb \
+		ath11k-firmware-ipq8071
 endef
 TARGET_DEVICES += redmi_ax6
 
