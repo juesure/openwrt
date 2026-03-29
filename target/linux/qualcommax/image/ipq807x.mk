70a3074a92f9e3bf416ea8b7201c3b349e1c7b27
juesure <249899074@qq.com>
2026-03-29 10:41:47 +0800

    Update ipq807x.mk
    Signed-off-by: juesure <249899074@qq.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -4,11 +4,12 @@ include $(INCLUDE_DIR)/image.mk
 DEVICE_VARS += QCOM_SOC_ROM QCOM_DTS_ROM QCOM_ART_ROM
 
 define Device/redmi_ax6
-$(call Device/xiaomi_ax3600)
-DEVICE_VENDOR := Redmi
-DEVICE_MODEL := AX6
-IMAGE_SIZE := 245760k
-DEVICE_PACKAGES := ipq-wifi-redmi_ax6
+  SOC := ipq8071
+  DEVICE_DTS := ipq8071-ax6
+  DEVICE_VENDOR := Redmi
+  DEVICE_MODEL := AX6
+  IMAGE_SIZE := 245760k
+  DEVICE_PACKAGES := ipq-wifi-redmi_ax6
 endef
 TARGET_DEVICES += redmi_ax6
 
